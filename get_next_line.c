aadea49d4f184f2bdf66c292ef241755487405f6
narlati <narlati@student.42.fr>
2016-11-16 20:10:17 +0100

    petit patch

--- get_next_line.c
+++ get_next_line.c
@@ -1,4 +1,73 @@
-int get_next_line(const int fd, char **line)
-{
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: narlati <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/16 12:49:07 by narlati           #+#    #+#             */
+/*   Updated: 2016/11/16 20:09:14 by narlati          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include "get_next_line.h"
+#include <unistd.h>
+#include "libft/libft.h"
+#include <fcntl.h>
+#include <stdio.h>
+
+void			ft_print_buffer(char *s)
+{
+	int i;
+
+	i = 0;
+	while (i < BUFF_SIZE)
+	{
+		ft_putchar(s[i]);
+		i++;
+	}
+}
+
+
+char			*new_alloc(char *s)
+{
+	int i;
+	char *s1;
+
+	i = 0;
+	while (s[i] != '\0' || s[i] != '\n')
+		i++;
+	s1 = ft_memalloc(sizeof(i) * i);
+	ft_memccpy(s1, s, '\n', i - 1);
+	return (s1);
+}
+
+int				get_next_line(const int fd, char **line)
+{
+	static char	*buffer = NULL;
+	int			ret;
+
+	if (buffer == NULL)
+		buffer = malloc(BUFF_SIZE);
+	ret = read(fd, buffer, BUFF_SIZE);
+	ft_print_buffer(buffer);
+	if (ret == -1)
+		return (-1);
+	else if (ret > 0)
+	{
+		buffer = new_alloc(buffer);
+		ft_print_buffer(buffer);
+	}
+	return (0);
+}
+
+int main (int argc, char **argv)
+{
+	int fd;
+	char *line;
+
+	fd = open(argv[1], O_RDWR);
+	printf("%d\n", get_next_line(fd, &line));
+	close (fd);
+	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,5 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: narlati <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/16 16:45:47 by narlati           #+#    #+#             */
+/*   Updated: 2016/11/16 19:15:22 by narlati          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_h
+# define GET_NEXT_LINE_H
+
+# define BUFF_SIZE 12
 
 int get_next_line(const int fd, char **line);
 
