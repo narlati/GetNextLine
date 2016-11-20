d78913c1163bf85b32d4e4f9560ad8a177816945
narlati <narlati@student.42.fr>
2016-11-20 16:07:28 +0100

    l

---
+++ libft/ft_strnjoin.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnjoin.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: narlati <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/20 13:33:24 by narlati           #+#    #+#             */
+/*   Updated: 2016/11/20 13:45:44 by narlati          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnjoin(char const *s1, char const *s2, size_t n)
+{
+	char *s3;
+
+	if (!(s3 = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2)))))
+   		return (NULL);
+	ft_strcpy(s3, s1);
+	ft_strncat(s3, s2, n);
+	return (s3);
+}
