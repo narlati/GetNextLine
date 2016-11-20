3275bf259959ae805c31411b377987e147dbef32
narlati <narlati@student.42.fr>
2016-11-20 17:42:36 +0100

    ajout liste dans le .h

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: narlati <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/16 16:45:47 by narlati           #+#    #+#             */
-/*   Updated: 2016/11/20 15:09:29 by narlati          ###   ########.fr       */
+/*   Updated: 2016/11/20 17:41:57 by narlati          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,12 @@
 
 # define BUFF_SIZE 5
 
-int get_next_line(const int fd, char **line);
+typedef struct	s_buffer
+{
+	struct s_	*next;
+	static char	buffer[BUFF_SIZE] = {0};
+}				t_buffer;
+
+int 			get_next_line(const int fd, char **line);
 
 #endif
