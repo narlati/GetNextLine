9d4fc9ea5c3c8c4182857de160f6e4805531a58e
narlati <narlati@student.42.fr>
2016-11-22 17:17:01 +0100

    avance

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: narlati <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/16 12:49:07 by narlati           #+#    #+#             */
-/*   Updated: 2016/11/21 16:09:00 by narlati          ###   ########.fr       */
+/*   Updated: 2016/11/22 17:16:44 by narlati          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,67 @@
 #include <fcntl.h>
 #include <stdio.h>
 
+t_buffer	*ft_init_list(const int fd, t_buffer *yy)
+{
+	if (yy == NULL)
+	{
+		yy = malloc(sizeof(yy));
+		yy->buffer = NULL;
+		yy->fd = fd;
+		yy->next = NULL;
+	}
+	else
+	{
+		while (yy != NULL && yy->fd != fd)
+		{
+			yy = yy->next;
+			if (yy == NULL)
+			{
+				yy = malloc(sizeof(yy));
+				yy->buffer = NULL;
+				yy->fd = fd;
+				yy->next = NULL;
+			}	
+		}
+	}
+	return (yy);
+}	
+
+int			test_retour_chariot(t_buffer *yy, char **line)
+{
+	char *tmp;
+	if (yy->buffer != NULL && (tmp = ft_strchr(*line, '\n')))
+	{	
+		**line = *ft_strjoin(*line, ft_strsub(yy->buffer, 0, tmp - yy->buffer));
+		yy->buffer = tmp + 1;
+		return (1);
+	}
+	return (0);
+	
+}
+
 int				get_next_line(const int fd, char **line)
 {
 	static t_buffer *tt = NULL;
 	int ret;
-	int p;
+	char buffer[BUFF_SIZE] = {0};
 
-	tt->buffer = &buffer[BUFF_SIZE];
-	ret = read(fd, tt->buffer, BUFF_SIZE);
+	ret = read(fd, buffer, BUFF_SIZE);
 	if ((ret == -1) || (fd == -1))
 		return (-1);
-	while (ret >= 0)
+	*line = ft_strnew(0);
+	tt = ft_init_list(fd, tt);
+	if (test_retour_chariot(tt, line))
+		return (1);
+	prin
+
+/*
+	if (ret > 0)
 	{
-		p = ft_solve(tt->buffer, line, ret);
-		if (p == 1)
-			return (1);
-		else if (p == 0)
-			return (0);
-		ret = read(fd, tt->buffer, BUFF_SIZE);
+		return(4);
 	}
+	*line = malloc(BUFF_SIZE + 1);
+*/
 	return (0);
 }
 
@@ -43,7 +85,10 @@ int main (int argc, char **argv)
 
 	int fd;
 	char *line;
+	int i = 0;
 
+	while (i <argc)
+		i++;
 	fd = open(argv[1], O_RDWR);
 	printf("%d\n", get_next_line(fd, &line));
 	free (line);
--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: narlati <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/16 16:45:47 by narlati           #+#    #+#             */
-/*   Updated: 2016/11/21 15:27:39 by narlati          ###   ########.fr       */
+/*   Updated: 2016/11/22 17:14:27 by narlati          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 5
+# define BUFF_SIZE 3
 
 typedef struct		s_buffer
 {
