d6f7f70f1d4c15f56eaac145252e875d8ca3de97
narlati <narlati@student.42.fr>
2016-11-27 15:22:39 +0100

    tada

--- get_next_line.c
+++ get_next_line.c
@@ -1,103 +1,69 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: narlati <marvin@42.fr>                     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/11/16 12:49:07 by narlati           #+#    #+#             */
-/*   Updated: 2016/11/24 14:25:24 by narlati          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "get_next_line.h"
 #include <unistd.h>
 #include "libft/libft.h"
 #include <fcntl.h>
-#include <stdio.h>
-#include <errno.h>  
 
-void		init_list(t_buffer **tt, int const fd)
+int		line_verif(char **line, char **tmp, int res, char **str)
 {
-	if (*tt == NULL)
+	*str = NULL;
+	if (res == 0 && ft_strlen(*tmp) > 0)
 	{
-		*tt = malloc(sizeof(tt));
-		(*tt)->fd = fd;
-		(*tt)->buffer = malloc(BUFF_SIZE);
-		(*tt)->next = NULL;
-	}
-}
-
-int			solver(t_buffer *tt, char **line, int ret)
-{
-	char string_buff[ret + 1];
-
-	if (ft_memchr(tt->buffer, '\n', ret))
-	{
-		size_t asdfghj = ft_memchr(tt->buffer, '\n', ret) - (void*)tt->buffer;
-		*line = ft_strjoin(*line, ft_strsub(tt->buffer, 0, asdfghj));
-		ft_memmove(tt->buffer, ft_memchr(tt->buffer, '\n', ret) + 1, BUFF_SIZE - asdfghj + 1);
+		*line = *tmp;
+		*tmp = NULL;
 		return (1);
 	}
-	else
-	{
-		ft_memcpy(string_buff, tt->buffer, ret);
-		string_buff[ret] = '\0';
-		*line = ft_strjoin(*line, string_buff);
-		if (ret == BUFF_SIZE)
-			return (42);
-		return (0);
-	}
-	return (42);
+	return (res);
 }
 
-int				get_next_line(const int fd, char **line)
+char	*read_line(char *tmp)
 {
-	static t_buffer *tt = NULL;
-	int ret;
-	int s;
+	int		t;
+	char	*line;
 
-	if (fd == -1)
+	t = 0;
+	while (tmp[t] != '\n')
+		t++;
+	line = (char *)malloc((t + 1) * sizeof(char));
+	line = ft_strncpy(line, tmp, t);
+	line[t] = '\0';
+	return (line);
+}
+
+char	*cpycat(char *s1, char *s2)
+{
+	char	*tmp;
+
+	tmp = NULL;
+	tmp = ft_memalloc(ft_strlen(s1) + ft_strlen(s2));
+	s1 ? tmp = ft_strcpy(tmp, s1) : NULL;
+	s1 ? tmp = ft_strncat(tmp, s2, ft_strlen(s2)) : NULL;
+	return (tmp);
+}
+							
+int		get_next_line(int const fd, char **line)
+{
+	static char		*str = NULL;
+	int				res;
+	char			*buf;
+	char			*tmp;
+
+	if (fd < 0 || !line || BUFF_SIZE < 1 || BUFF_SIZE > 10000000)
 		return (-1);
-	*line = ft_strnew(0);
-	init_list(&tt, fd);
-	while ((ret = read(fd, tt->buffer, BUFF_SIZE)))
+	buf = ft_strnew(BUFF_SIZE + 1);
+		if (str == NULL)
+			str = ft_memalloc(BUFF_SIZE);
+	tmp = ft_strncpy(ft_memalloc(BUFF_SIZE), str, BUFF_SIZE);
+	while (!(ft_strchr(tmp, '\n')))
 	{
-		if (ret < 0)
-			return (-1);
-		s = solver(tt, line, ret);
-		if (s == 1)
-			return (1);	
-		else if (s == 0)
-			return (0);
-		else
-			;
+		if ((res = read(fd, buf, BUFF_SIZE)) < 1)
+			return (line_verif(line, &tmp, res, &str));
+		buf[res] = '\0';
+		tmp = cpycat(tmp, buf);
 	}
-//	free_and_del(&tt);
-//	tt = NULL;
-	return (0);
-}
-
-
-int main (int argc, char **argv)
-{
-
-	int fd;
-	char *line;
-	int i = 0;
-	int s;
-
-	while (i <argc)
-		i++;
-	fd = open(argv[1], O_RDWR);
-	s = get_next_line(fd, &line);
-	while (s > 0)
-	{
-		printf("line = %s\n", line);
-		printf("GNL = %d\n", s);
-		s = get_next_line(fd, &line);
-	}
-	free (line);
-	close (fd);
-	return (0);
+	*line = read_line(tmp);
+	if (ft_strchr(tmp, '\n'))
+		str = ft_strncpy(str, ft_strchr(tmp, '\n') + 1, BUFF_SIZE);
+	free(tmp);
+	free(buf);
+	return (1);
 }
