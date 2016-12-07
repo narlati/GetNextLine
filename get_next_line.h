2c1796d2d5d269c758e24f009af57301929a1f41
narlati <narlati@student.42.fr>
2016-12-07 17:15:16 +0100

    pour compiler

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,19 @@
 /*   By: narlati <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/16 16:45:47 by narlati           #+#    #+#             */
-/*   Updated: 2016/12/06 17:10:30 by narlati          ###   ########.fr       */
+/*   Updated: 2016/12/07 14:25:13 by narlati          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+#include "libft/libft.h"
+#include <unistd.h>
+
 # define BUFF_SIZE 1
+# define MAX_BUFF_SIZE 20000
+# define MAX_FD 2064
 
 int 			get_next_line(const int fd, char **line);
 
