6e698bb47efe9163d19d47254fc33f5377d490d6
narlati <narlati@student.42.fr>
2016-12-07 17:06:08 +0100

    rolalal

--- get_next_line.h
+++ get_next_line.h
@@ -6,21 +6,14 @@
 /*   By: narlati <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/16 16:45:47 by narlati           #+#    #+#             */
-/*   Updated: 2016/11/24 14:25:30 by narlati          ###   ########.fr       */
+/*   Updated: 2016/12/06 17:10:30 by narlati          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 5
-
-typedef struct		s_buffer
-{
-	char			*buffer;
-	int				fd;
-	struct s_buffer	*next;
-}					t_buffer;
+# define BUFF_SIZE 1
 
 int 			get_next_line(const int fd, char **line);
 
